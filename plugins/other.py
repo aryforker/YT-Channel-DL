588466da70ad5843de9b071c949b40d6b58c817f
aryforker <97401553+aryforker@users.noreply.github.com>
2022-01-12 12:07:16 +0330

    Update other.py

--- plugins/other.py
+++ plugins/other.py
@@ -5,7 +5,7 @@ import asyncio
 @Client.on_message(filters.command("start"))
 async def start_msg(client, message):
 	await message.reply_text(
-		f"درود بر {message.from_user.mention} من بات دانلودر nlm هستم و میتونم فایل مورد علاقتو از یوتیوب دانلود کنم,",
+		f"درود بر {message.from_user.mention} .\n من بات دانلودر NLM هستم و میتونم فایل مورد علاقتو از یوتیوب دانلود کنم,",
 		reply_markup=InlineKeyboardMarkup(
 				[[
 					InlineKeyboardButton("🛠 کمک", callback_data=f"کمک"),
@@ -19,7 +19,7 @@ async def cb_handler(client, update):
 	cb_data = update.data
 	
 	if "کمک" in cb_data:
-		await update.message.edit_text("فقط کافیه لینک مورد علاقتو به فرم روروبر برام ارسال کنی و کنارش یکی از کلمات(Audio/Video) رو بذاری تا بتونم بفهمم فیلم اون رو میخوای یا اهنگشو.\nبرای مثال :\n`https://www.youtube.com/watch?v=Fifk9DfJt6o video",
+		await update.message.edit_text("فقط کافیه لینک مورد علاقتو به فرم روروبر برام ارسال کنی و کنارش یکی از کلمات(Audio/Video) رو بذاری تا بتونم بفهمم فیلم اون رو میخوای یا اهنگشو.\nبرای مثال :\n`https://www.youtube.com/watch?v=Fifk9DfJo video",
 			reply_markup=InlineKeyboardMarkup(
 				[[
 					InlineKeyboardButton("🧰 درباره ما", callback_data=f"درباره ما"),
