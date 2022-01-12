bc2a5b9e94cca75d2fca3cd44643587966f5055c
aryforker <97401553+aryforker@users.noreply.github.com>
2022-01-12 11:52:38 +0330

    Update other.py

--- plugins/other.py
+++ plugins/other.py
@@ -18,23 +18,23 @@ async def start_msg(client, message):
 async def cb_handler(client, update):
 	cb_data = update.data
 	
-	if "help" in cb_data:
+	if "کمک" in cb_data:
 		await update.message.edit_text("فقط کافیه لینک مورد علاقتو به فرم روروبر برام ارسال کنی و کنارش یکی از کلمات(Audio/Video) رو بذاری تا بتونم بفهمم فیلم اون رو میخوای یا اهنگشو.\nبرای مثال :\n`https://www.youtube.com/watch?v=Fifk9DfJt6o video",
 			reply_markup=InlineKeyboardMarkup(
 				[[
 					InlineKeyboardButton("🧰 درباره ما", callback_data=f"درباره ما"),
-					InlineKeyboardButton("🔙 کمک", callback_data=f"کمک")
+					InlineKeyboardButton("🔙 برشگت", callback_data=f"برگشت")
 				]]
 			))
-	elif "about" in cb_data:
-		await update.message.edit_text("Language: Python\nFramework: Pyrogram\nEngine: YTDL",
+	elif "درباره ما" in cb_data:
+		await update.message.edit_text("چنل اصلی ما : @NLM_News \nبقیه بات ها :@The_Aryana_PY\nسازنده : @aryana_gha",
 			reply_markup=InlineKeyboardMarkup(
 				[[
                                         InlineKeyboardButton("🛠 کمک", callback_data=f"کمک"),
-					InlineKeyboardButton("🧰 درباره ما", callback_data=f"درباره ما")
+					InlineKeyboardButton("🔙 برشگت", callback_data=f"برگشت")
 				]]
 			))
-	elif "back" in cb_data:
+	elif "برکشت" in cb_data:
 		await update.message.edit_text(f"سلامی دوباره بر {update.from_user.mention}, برای دریافت راهنمایی گزینه کمک رو بزنید  ",
 			reply_markup=InlineKeyboardMarkup(
 				[[
